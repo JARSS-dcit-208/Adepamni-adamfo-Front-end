85240547b5523fb939dce12def3163946cfa4134
RYO-NA <137723039+RYO-NA@users.noreply.github.com>
2023-11-19 18:10:35 +0000

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <!-- <full-page-progress-loader/> -->
+    <v-snackbar
+      v-model="successNotificationVisible"
+      top
+      right
+      :timeout="2000"
+      location="top"
+      color="primary"
+    >
+      {{notifications.success}}
+    </v-snackbar>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+const successNotificationVisible = ref(false)
+const notifications = ref({success: null, error: null})
+
+function successNotification(message){
+  notifications.value.success = message
+  successNotificationVisible.value = true
+}
+provide('successNotification', successNotification)
+</script>
+
+<style lang="scss">
+.custom-primary-button{
+  color: white;
+  background: blue;
+}
+.app{
+  background: #f8f6ef;
+  min-height: 100vh;
+}
+</style>
